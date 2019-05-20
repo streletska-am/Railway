1729c8df31101ec31947c713a5cef96336183881
Vitalii Chereshnia <vitalii.chereshnia@thomsonreuters.com>
2019-05-20 03:47:15 +0300

    Fix json imports and scripts

--- src/main/resources/sql/create.sql
+++ src/main/resources/sql/create.sql
@@ -19,7 +19,7 @@ USE `railway_system` ;
 DROP TABLE IF EXISTS `railway_system`.`user` ;
 
 CREATE TABLE IF NOT EXISTS `railway_system`.`user` (
-  `id` CHAR(36) NOT NULL,
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
   `email` VARCHAR(255) NOT NULL,
   `password` VARCHAR(35) NOT NULL,
   `name` VARCHAR(35) NOT NULL,
@@ -38,10 +38,10 @@ DEFAULT CHARACTER SET = utf8;
 DROP TABLE IF EXISTS `railway_system`.`price` ;
 
 CREATE TABLE IF NOT EXISTS `railway_system`.`price` (
-  `id` CHAR(36) NOT NULL,
-  `compartment_factor` FLOAT NOT NULL,
-  `deluxe_factor` FLOAT NOT NULL,
-  `berth_factor` FLOAT NOT NULL,
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `compartmentFactor` FLOAT NOT NULL,
+  `deluxeFactor` FLOAT NOT NULL,
+  `berthFactor` FLOAT NOT NULL,
   PRIMARY KEY (`id`))
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8;
@@ -53,7 +53,7 @@ DEFAULT CHARACTER SET = utf8;
 DROP TABLE IF EXISTS `railway_system`.`station` ;
 
 CREATE TABLE IF NOT EXISTS `railway_system`.`station` (
-  `id` CHAR(36) NOT NULL,
+  `id` INT NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(45) NOT NULL,
   PRIMARY KEY (`id`))
 ENGINE = InnoDB;
@@ -65,29 +65,29 @@ ENGINE = InnoDB;
 DROP TABLE IF EXISTS `railway_system`.`route` ;
 
 CREATE TABLE IF NOT EXISTS `railway_system`.`route` (
-  `id` CHAR(36) NOT NULL,
-  `from_time` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `to_time` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `price_id` CHAR(36) NOT NULL,
-  `from_id` CHAR(36) NOT NULL,
-  `to_id` CHAR(36) NOT NULL,
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `fromTime` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `toTime` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `priceId` INT(11) NOT NULL,
+  `fromId` INT NOT NULL,
+  `toId` INT NOT NULL,
   `distance` DOUBLE NOT NULL,
   PRIMARY KEY (`id`),
-  INDEX `fk_route_price1_idx` (`price_id` ASC),
-  INDEX `fk_route_station1_idx` (`from_id` ASC),
-  INDEX `fk_route_station2_idx` (`to_id` ASC),
+  INDEX `fk_route_price1_idx` (`priceId` ASC),
+  INDEX `fk_route_station1_idx` (`fromId` ASC),
+  INDEX `fk_route_station2_idx` (`toId` ASC),
   CONSTRAINT `fk_route_price1`
-    FOREIGN KEY (`price_id`)
+    FOREIGN KEY (`priceId`)
     REFERENCES `railway_system`.`price` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_route_station1`
-    FOREIGN KEY (`from_id`)
+    FOREIGN KEY (`fromId`)
     REFERENCES `railway_system`.`station` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_route_station2`
-    FOREIGN KEY (`to_id`)
+    FOREIGN KEY (`toId`)
     REFERENCES `railway_system`.`station` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
@@ -101,15 +101,15 @@ DEFAULT CHARACTER SET = utf8;
 DROP TABLE IF EXISTS `railway_system`.`train` ;
 
 CREATE TABLE IF NOT EXISTS `railway_system`.`train` (
-  `id` CHAR(36) NOT NULL,
-  `route_id` CHAR(36) NOT NULL,
-  `compartment_free` INT(11) NOT NULL,
-  `deluxe_free` INT(11) NOT NULL,
-  `berth_free` INT(11) NOT NULL,
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `routeId` INT NOT NULL,
+  `compartmentFree` INT(11) NOT NULL,
+  `deluxeFree` INT(11) NOT NULL,
+  `berthFree` INT(11) NOT NULL,
   PRIMARY KEY (`id`),
-  INDEX `train_route_idx` (`route_id` ASC),
+  INDEX `train_route_idx` (`routeId` ASC),
   CONSTRAINT `train_route`
-    FOREIGN KEY (`route_id`)
+    FOREIGN KEY (`routeId`)
     REFERENCES `railway_system`.`route` (`id`)
     ON DELETE CASCADE
     ON UPDATE CASCADE)
@@ -123,27 +123,28 @@ DEFAULT CHARACTER SET = utf8;
 DROP TABLE IF EXISTS `railway_system`.`request` ;
 
 CREATE TABLE IF NOT EXISTS `railway_system`.`request` (
-  `id` CHAR(36) NOT NULL,
-  `user_id` CHAR(36) NOT NULL,
-  `train_id` CHAR(36) NOT NULL,
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `userId` INT(11) NOT NULL,
+  `trainId` INT(11) NOT NULL,
   `type` ENUM('C', 'L', 'B') NOT NULL,
   `price` INT(11) NOT NULL,
   PRIMARY KEY (`id`),
-  INDEX `passenger_idx` (`user_id` ASC),
-  INDEX `invoice_train_idx` (`train_id` ASC),
+  INDEX `passenger_idx` (`userId` ASC),
+  INDEX `invoice_train_idx` (`trainId` ASC),
   CONSTRAINT `passenger`
-    FOREIGN KEY (`user_id`)
+    FOREIGN KEY (`userId`)
     REFERENCES `railway_system`.`user` (`id`)
     ON DELETE CASCADE
     ON UPDATE CASCADE,
   CONSTRAINT `invoice_train`
-    FOREIGN KEY (`train_id`)
+    FOREIGN KEY (`trainId`)
     REFERENCES `railway_system`.`train` (`id`)
     ON DELETE CASCADE
     ON UPDATE CASCADE)
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8
 COMMENT = '\n';
+
 
 SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
